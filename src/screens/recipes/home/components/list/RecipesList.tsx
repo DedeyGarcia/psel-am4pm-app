4aa60ce828751863a4cb2ba3caa64d95bf72045b
Andrey Garcia <andreylucas.garcia@gmail.com>
2026-06-09 20:46:30 -0300

    feat: improve recipes list ui with scroll to offset

--- src/screens/recipes/home/components/list/RecipesList.tsx
+++ src/screens/recipes/home/components/list/RecipesList.tsx
@@ -1,14 +1,29 @@
-import { FlashList } from '@shopify/flash-list';
+import { FlashList, FlashListRef } from '@shopify/flash-list';
 import RecipeCard from '../recipeCard/RecipeCard';
 import { Recipe } from '../../../../../../types/recipe';
 import RecipesListSeparatorComponent from './RecipesListSeparatorComponent';
 import RecipesListEmptyComponent from './RecipesListEmptyComponent';
+import { useEffect, useRef } from 'react';
+import { useRecipeFiltersStore } from '../../../../../store/recipeFiltersStore';
 
 interface RecipesListProps {
   filteredRecipes: Recipe[];
 }
 
 export default function RecipesList({ filteredRecipes }: RecipesListProps) {
+  const listRef = useRef<FlashListRef<Recipe>>(null);
+  const searchQuery = useRecipeFiltersStore(s => s.searchQuery);
+  const selectedCategories = useRecipeFiltersStore(s => s.selectedCategories);
+
+  useEffect(() => {
+    const id = requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        listRef.current?.scrollToOffset({ offset: 0, animated: false });
+      });
+    });
+    return () => cancelAnimationFrame(id);
+  }, [searchQuery, selectedCategories]);
+
   const recipesListRenderItem = ({ item }: { item: Recipe }) => {
     return <RecipeCard item={item} />;
   };
@@ -17,6 +32,7 @@ export default function RecipesList({ filteredRecipes }: RecipesListProps) {
 
   return (
     <FlashList
+      ref={listRef}
       data={filteredRecipes}
       renderItem={recipesListRenderItem}
       keyExtractor={recipesListKeyExtractor}
